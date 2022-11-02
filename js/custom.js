c292ba6772953fd2b6c665d690bc1e770b8aee9a
sahildgrooar <sahil.digitalrooar@gmail.com>
2022-11-02 10:48:50 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -99,6 +99,28 @@ $('.banproductslide').slick({
   ]
 });
 
+$('.productBoxMain').slick({
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: false,
+  dots:false,
+
+  responsive: [
+    {
+
+      breakpoint: 568, 
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows:false,
+        dots:true,
+        autoplay:true,
+      }
+    },
+  ]
+});
+
+
 
   /* Mobile filter  & shorting */
 
